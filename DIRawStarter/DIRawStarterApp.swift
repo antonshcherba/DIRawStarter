d586317a1c0da64a85db7e25002823a55fdbfc94
Anton Shcherba <ShcherbaA@ae.com>
2023-08-21 20:48:53 +0200

    Initial Commit

---
+++ DIRawStarter/DIRawStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIRawStarterApp.swift
+//  DIRawStarter
+//
+//  Created by Anton Shcherba on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DIRawStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
